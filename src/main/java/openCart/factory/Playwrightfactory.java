a6f1640b06e9b8d417cce48b9048a5b57aff83f5
Priti <pritipradhan2014@gmail.com>
2025-07-25 19:01:51 +0530

    headless true

--- src/main/java/openCart/factory/Playwrightfactory.java
+++ src/main/java/openCart/factory/Playwrightfactory.java
@@ -47,20 +47,20 @@ public class Playwrightfactory {
             case "chromium" :
                 System.out.println("Launching chromium!!");
                 //browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
-                tlBrowser.set(tlPlaywright.get().chromium().launch(new BrowserType.LaunchOptions().setHeadless(false)));
+                tlBrowser.set(tlPlaywright.get().chromium().launch(new BrowserType.LaunchOptions().setHeadless(true)));
                 break;
             case "safari" :
                 //browser = playwright.webkit().launch(new BrowserType.LaunchOptions().setHeadless(false));
-                tlBrowser.set(tlPlaywright.get().webkit().launch(new BrowserType.LaunchOptions().setHeadless(false)));
+                tlBrowser.set(tlPlaywright.get().webkit().launch(new BrowserType.LaunchOptions().setHeadless(true)));
                 break;
             case "firefox" :
                 //browser = playwright.firefox().launch(new BrowserType.LaunchOptions().setHeadless(false));
-                tlBrowser.set(tlPlaywright.get().firefox().launch(new BrowserType.LaunchOptions().setHeadless(false)));
+                tlBrowser.set(tlPlaywright.get().firefox().launch(new BrowserType.LaunchOptions().setHeadless(true)));
                 break;
             case "chrome" :
                 System.out.println("Launching chrome");
                 //browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setChannel("chrome").setHeadless(false));
-                tlBrowser.set(tlPlaywright.get().chromium().launch(new BrowserType.LaunchOptions().setHeadless(false)));
+                tlBrowser.set(tlPlaywright.get().chromium().launch(new BrowserType.LaunchOptions().setHeadless(true)));
                 break;
             default:
                 System.out.println("Browsername not found!!");
